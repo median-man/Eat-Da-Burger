abf0fe50a9a9410ea34bc43da676dc2ab67fd719
John Desrosiers <johndrunner@yahoo.com>
2017-09-26 06:31:09 -0700

    initial selectAll function definition added

--- config/orm.js
+++ config/orm.js
@@ -3,9 +3,22 @@
 // =====================================================
 const Connection = require("./connection.js");
 
-// TODO Query to select all data in burgers table. Returns a promise.
+// Query to select all data in table. Returns a promise passing for the selected data.
 function selectAll(table) {
+	// create a promise
+	return new Promise( ( resolve, reject ) => {
+		let sql = "SELECT * FROM ??";
 
+		// run query to get all data in table
+		Connection.query( sql, table, ( err, data ) => {
+
+			// run rejection callback on error
+			if(err) return reject(err);
+
+			// pass data to success callback
+			return resolve(data);
+		});
+	} );
 }
 
 // TODO Query to add a burger to the burgers table. Returns a prmise.
@@ -17,3 +30,5 @@ function insertOne(table, data) {
 function updateOne(table, primaryKey, data) {
 	
 }
+
+selectAll("burgers").then(console.log).catch(console.log);
