b07d2471d853bb48a83ee2ce98a249d1a109fb5b
John Desrosiers <johndrunner@yahoo.com>
2017-09-26 06:45:56 -0700

    added initial definition of insertOne function

--- config/orm.js
+++ config/orm.js
@@ -3,7 +3,8 @@
 // =====================================================
 const Connection = require("./connection.js");
 
-// Query to select all data in table. Returns a promise passing for the selected data.
+// Query to select all data in table. Returns a promise passing 
+// for the selected data.
 function selectAll(table) {
 	// create a promise
 	return new Promise( ( resolve, reject ) => {
@@ -21,9 +22,21 @@ function selectAll(table) {
 	} );
 }
 
-// TODO Query to add a burger to the burgers table. Returns a prmise.
+// Query to add a burger to the burgers table passing OkPacket data object
+// to the success promise. Returns a promise. data is an object where keys 
+// correspond to field names and values are the values to place in the fields
 function insertOne(table, data) {
+	return new Promise( ( resolve, reject ) => {
 
+		// initialize sql statement
+		let sql = "INSERT INTO ?? SET ?";
+
+		// run query to get all data in table
+		Connection.query( sql, [table, data], ( err, data ) => {
+			if(err) return reject(err);
+			return resolve(data);
+		});
+	} );
 }
 
 // TODO Query to update one record in burgers table
@@ -31,4 +44,8 @@ function updateOne(table, primaryKey, data) {
 	
 }
 
-selectAll("burgers").then(console.log).catch(console.log);
+// selectAll("burgers").then(console.log).catch(console.log);
+insertOne("burgers", {
+	burger_name: "Anchovy Delight",
+	devoured: false
+}).then(console.log).catch(console.log);
