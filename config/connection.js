dfaa1e6068417b4867b616856d73bc961fbe80b1
John Desrosiers <johndrunner@yahoo.com>
2017-09-26 05:46:57 -0700

    configure and export burgers_db connection

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,19 @@
-// TODO export connection to mysql
+// =====================================================
+// connection.js
+//
+// This module contains configuration parameters for
+// connecting to the burgers_db and exports the mysql
+// connection object for the db.
+// =====================================================
+const Mysql = require("mysql");
+
+// db connection configuration
+const config = {
+	host: "localhost",
+	user: "root",
+	password: "rictor",
+	database: "burgers_db"
+};
+
+// export connection to burgers_db
+module.exports = mysql.createConnection(config);
