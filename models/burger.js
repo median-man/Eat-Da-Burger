cbdeeeba346d2b3e445d8c2317649bbfaef9f262
John Desrosiers <johndrunner@yahoo.com>
2017-09-27 05:59:35 -0700

    remove timestamp in burger.js add method

--- models/burger.js
+++ models/burger.js
@@ -8,23 +8,17 @@ const ORM = require("../config/orm.js");
 
 // Gets all records from burgers table. Returns a promise.
 function getAllBurgers() {
-	// uses ORM.selectAll to get all the data from burgers table
 	return ORM.selectAll("burgers");
 }
 
 // Adds burger to burgers table. Returns a promise.
-function add(name, devoured = false, timestamp = void 0) {
+function add(name, devoured = false) {
 
 	// object with keys corresponding to burgers table
 	let newBurger = {
 		burger_name: name,
 		devoured: devoured
 	};
-
-	// add date field if timestamp not null
-	if(timestamp) newBurger.date = timestamp;
-	
-	// uses ORM.insertOne
 	return ORM.insertOne( "burgers", newBurger);
 }
 
