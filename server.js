8e857836ac84d56772902ffd41179276c0864bc9
John Desrosiers <johndrunner@yahoo.com>
2017-09-27 07:08:37 -0700

    implemented routes and handlebars in server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 const Express = require("express");
 const BodyParser = require("body-parser");
 const MethodOverride = require("method-override");
+const Exphbs = require("express-handlebars");
+const Routes = require("./controllers/burgers_controller.js");
 
 let app = Express();
 let PORT = process.env.PORT || 8080;
@@ -11,9 +13,19 @@ let PORT = process.env.PORT || 8080;
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(Express.static("public"));
 
-app.use(MethodOverride("_method"));
-
 // Parse application/x-www-form-urlencoded
 app.use(BodyParser.urlencoded({ extended: false }));
 
-app.listen(PORT);
+// implement method override middleware
+app.use(MethodOverride("_method"));
+
+// Setup Handlebars
+app.engine("handlebars", Exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// connecting router
+app.use("/", Routes);
+
+app.listen(PORT, () => {
+	console.log("Server listening on: http://localhost:%s", PORT);
+});
