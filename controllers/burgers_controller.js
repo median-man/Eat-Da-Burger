f784e55a0ea5fbf02db6e448239b56aa129b1fb6
John Desrosiers <johndrunner@yahoo.com>
2017-09-27 07:07:59 -0700

    added main get route to controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,10 +1,32 @@
 // =====================================================
 // DEPENDENCIES
 // =====================================================
+
 const Express = require("express");
-const Burger = require("../models/buruger.js");
+const Burger = require("../models/burger.js"); // model
 
-// TODO create create router
+// =====================================================
+// ROUTE HANDLERS
+// =====================================================
 
+// Displays the main page including burgers and add burger form
+function renderMain(response) {
+	Burger.getAllBurgers()
+		.then((burgers) => {
+
+			// render the page
+			response.render("index", { burgers: burgers });
+		});
+}
+
+// =====================================================
+// ROUTING
+// =====================================================
+
+let router = Express.Router();
+
+// main view
+router.get("/", (req, res) => { renderMain(res); });
 
 // TODO export router
+module.exports = router;
