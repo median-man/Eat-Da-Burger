1b4ac71495266a7eccaead5f38839af222fd6afb
John Desrosiers <johndrunner@yahoo.com>
2017-09-28 04:31:56 -0700

    add burger api route and handling added

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,6 +9,12 @@ const Burger = require("../models/burger.js"); // model
 // ROUTE HANDLERS
 // =====================================================
 
+// Handles a post request to add a new burger to the db.
+function addBurger(req, res) {
+	Burger.add(req.body.burger_name)
+		.then( () => { res.redirect("/"); });		
+}
+
 // Handles a put request to devour a burger and redirects to root.
 function devourBurger(req, res) {
 	Burger.devour(req.params.id)
@@ -34,6 +40,9 @@ let router = Express.Router();
 
 // main view
 router.get("/", (req, res) => { renderMain(res); });
+
+// add burger
+router.post("/", addBurger);
 
 // devour api
 router.put("/:id", devourBurger);
